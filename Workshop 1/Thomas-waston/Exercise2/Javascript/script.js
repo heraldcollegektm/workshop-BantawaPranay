70c53ee1cdd977e858af9ceea113baaab31ba198
BantawaPranay <107391392+BantawaPranay@users.noreply.github.com>
2022-06-23 05:14:39 +0000

    js

---
+++ Workshop 1/Thomas-waston/Exercise2/Javascript/script.js
@@ -0,0 +1,15 @@
+// let messages = "pranay";
+// setTimeout(wakeUpUser, 500)
+// function wakeUpUser() {
+    // alert("Are you going to star at this boring page forever")
+
+    // document.write("pranay");
+    // document.write(34 + 3);
+    // prompt("Are you busy?")
+
+    let firstName = "Pranay Bantawa"
+    console.log(firstName)
+    console.log(firstName)
+    console.log(firstName)
+    console.log(firstName)
+    console.log(firstName)
